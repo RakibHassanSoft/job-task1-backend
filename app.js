cc864269a7ecfc07415651f230f6c065e6f7e123
RakibHassanSoft <rakibreings32@gmail.com>
2025-08-16 02:38:26 +0600

    text 3

--- app.js
+++ app.js
@@ -14,16 +14,10 @@ const allowedOrigins = [
 
 // ===== Middlewares =====
 app.use(cors({
-  origin: function(origin, callback) {
-    // allow requests with no origin (like Postman)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
+  origin: "*",          // <-- allow all origins
+  credentials: true,    // allow cookies
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use(express.json());
